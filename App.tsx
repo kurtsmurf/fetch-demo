13927ed13e6b39cc12d985f3325e3a0a0e1c843c
Chris Smith <chrisronsmith@gmail.com>
2021-06-26 14:47:19 -0400

    Hook up yes/no API

--- App.tsx
+++ App.tsx
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import axios from "axios";
 
 export default function App() {
-  const [response, setResponse] = useState('')
+  const [answer, setAnswer] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const fetchAnAnswer = () => {
-    fetch("https://yesno.wtf/api")
-      .then(response => response.answer)
-      .then(setResponse)
-  }
+    setLoading(true);
 
-  useEffect(fetchAnAnswer, []) 
+    axios("https://yesno.wtf/api")
+      .then((response) => response.data.answer)
+      .then(setAnswer)
+      .then(() => setLoading(false));
+  };
+
+  useEffect(fetchAnAnswer, []);
+
+  const Answer = () => (
+    <View style={{
+      height: 200,
+      flex: 2,
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      {loading ? <ActivityIndicator /> : <Text
+        style={{
+          fontSize: 100,
+        }}
+      >
+        {answer}
+      </Text>}
+    </View>
+  );
+
+  const NextButton = () => (
+    <View       style={{
+        flex: 1
+    }}
+>
+      <Button
+        disabled={loading}
+        title="next"
+        onPress={fetchAnAnswer}
+      />
+    </View>
+  )
 
   return (
     <View style={styles.container}>
-      <Text>{response}</Text>
+      <Answer />
+      <NextButton />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,8 +66,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
