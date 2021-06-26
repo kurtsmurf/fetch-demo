8d2fd47e4ed7491847abc969d6d1e38eb47bb157
Chris Smith <chrisronsmith@gmail.com>
2021-06-26 13:49:09 -0400

    First crack - broken

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [response, setResponse] = useState('')
+
+  const fetchAnAnswer = () => {
+    fetch("https://yesno.wtf/api")
+      .then(response => response.answer)
+      .then(setResponse)
+  }
+
+  useEffect(fetchAnAnswer, []) 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{response}</Text>
       <StatusBar style="auto" />
     </View>
   );
