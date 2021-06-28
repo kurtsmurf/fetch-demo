39cc1c49ff8c68bbcae286a9a8e1ecd1ad27fddc
Chris Smith <chrisronsmith@gmail.com>
2021-06-28 14:27:53 -0400

    Pre-fetch images

--- useYesNoAPI.tsx
+++ useYesNoAPI.tsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Image } from "react-native";
 
 export const useYesNoAPI = () => {
   const [answer, setAnswer] = useState("");
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(true);
 
-  const fetchAnAnswer = () => {
+  const fetchAnswer = () => {
     setLoading(true);
 
-    axios("https://yesno.wtf/api")
-      .then((response) => response.data)
-      .then((data) => {
-        setAnswer(data.answer);
-        setImage(data.image);
-      })
-      .then(() => setLoading(false));
+    const fetchAnswerAsync = async () => {
+      const response = await axios("https://yesno.wtf/api")
+      await Image.prefetch(response.data.image)
+      setAnswer(response.data.answer);
+      setImage(response.data.image);
+      setLoading(false);
+    }
+
+    fetchAnswerAsync()
   };
 
-  useEffect(fetchAnAnswer, []);
+  useEffect(fetchAnswer, []);
 
-  return { answer, image, loading, next: fetchAnAnswer };
+  return { answer, image, loading, next: fetchAnswer };
 };
