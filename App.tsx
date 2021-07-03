9e2094e95388deac69baee85b3cb30720a8fe8ec
Kurt <chrisronsmith@gmail.com>
2021-07-02 21:45:27 -0400

    Refactor App.tsx

--- App.tsx
+++ App.tsx
@@ -2,63 +2,83 @@ import React from "react";
 import {
   ActivityIndicator,
   Button,
+  Image,
   StyleSheet,
   Text,
   View,
-  Image
 } from "react-native";
 import { useYesNoAPI } from "./useYesNoAPI";
 
 export default function App() {
-   const { answer, image, loading, next } = useYesNoAPI()
+  const { answer, image, loading, next } = useYesNoAPI();
 
-  const Answer = () => {
-    const AnswerText = () => (<Text
-      style={{
-        fontSize: 100
-      }}
-    >
-      {answer}
-    </Text>);
-
-    return <View
-      style={{
-        height: 200,
-        flex: 2,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {loading ? <ActivityIndicator size="large" color="blue" /> : <AnswerText />}
-    </View>;
-  };
-
-  const NextButton = () => (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <Button
-        disabled={loading}
-        title="next"
-        onPress={next}
-      />
-    </View>
-  );
-
-  return (
-    <View style={styles.container}>
-      <Image 
+  function AnswerImage() {
+    return (
+      <Image
         source={{
-          uri: image
+          uri: image,
         }}
         style={{
           flex: 2,
           height: 300,
-          width: '100%'
+          width: "100%",
         }}
       />
+    );
+  }
+
+  function Answer() {
+    function AnswerText() {
+      return (
+        <Text
+          style={{
+            fontSize: 100,
+          }}
+        >
+          {answer}
+        </Text>
+      );
+    }
+
+    function ActivityIndicatorOrText() {
+      return loading
+        ? <ActivityIndicator size="large" color="blue" />
+        : <AnswerText />;
+    }
+
+    return (
+      <View
+        style={{
+          height: 200,
+          flex: 2,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicatorOrText />
+      </View>
+    );
+  }
+
+  function NextButton() {
+    return (
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <Button
+          disabled={loading}
+          title="next"
+          onPress={next}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <AnswerImage />
       <Answer />
       <NextButton />
     </View>
